1485a1751c8587042a792bf8357114eb5af932d6
srfzm <saulrefoyo@gmail.com>
2021-10-26 13:37:47 +0200

    Añadido el Servlet MostrarDatosServ que redirige al servlert correspondiente indicandole la tabla a la que se quiere acceder.

---
+++ src/main/java/ejercicioServlets/servlets/MostrarDatosServ.java
@@ -0,0 +1,58 @@
+package ejercicioServlets.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MostrarDatosServ
+ */
+@WebServlet("/MostrarDatos")
+public class MostrarDatosServ extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MostrarDatosServ() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String tabla = request.getParameter("table");
+		if(tabla!=null)
+		{
+			if(tabla.equals("empleado"))
+				request.getRequestDispatcher("/MostrarEmpleados").forward(request, response);
+			else if(tabla.equals("departamento"))
+				request.getRequestDispatcher("/MostrarDepartamentos").forward(request, response);
+		}
+			
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		String tabla = request.getParameter("table");
+		if(tabla!=null)
+		{
+			if(tabla.equals("empleado"))
+				request.getRequestDispatcher("/MostrarEmpleados").forward(request, response);
+			else if(tabla.equals("departamento"))
+				request.getRequestDispatcher("/MostrarDepartamentos").forward(request, response);
+		}
+	}
+
+}
